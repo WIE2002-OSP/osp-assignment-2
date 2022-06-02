841db0a0c52934cc5f77024f347207d84e7eadd7
My name <me@exmaple.com>
2022-06-02 13:43:14 +0800

    Change colour theme
    I put the colour code inside doc

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials.*/
-## Required to be change then 
+## Required to be change then
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'trial');
+define('DB_NAME', 'osp_database');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
